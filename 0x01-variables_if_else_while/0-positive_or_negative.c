079e00b397b0fb945d0b8cdddb9693e9f4d3a368
Teboho Mosuhli <70834953+Mosuhli-Coder@users.noreply.github.com>
2023-06-19 15:42:45 +0200

    whitespacig

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0.
 */
-int main(void) 
+int main(void)
 {
 	int n;
 
@@ -21,6 +21,6 @@ int main(void)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-	
+
 	return (0);
 }
